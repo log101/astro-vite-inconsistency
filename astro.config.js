205f3c2fb19c23fa7aa9eb4c5f22471dafdad2fb
log101 <ffrknerdm@gmail.com>
2024-03-04 10:56:03 +0300

    init project

---
+++ astro.config.js
@@ -0,0 +1,32 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+export default {
+	renderers: [],
+	vite: {
+		plugins: [
+			{
+				name: '@astrojs/svg-test',
+				enforce: 'pre',
+				apply: 'serve',
+				async load(id = '') {
+					if (id.endsWith('.svg')) {
+						const asset = {
+							type: 'asset',
+							name: path.basename(id),
+							source: await fs.readFile(id, 'utf8'),
+						};
+
+						const referenceId = asset.name;
+
+						const reference = `import.meta.ROLLUP_FILE_URL_${referenceId}`;
+
+						console.log('load', { id, asset, referenceId, reference });
+
+						return `console.log(import.meta); export default ${reference};`;
+					}
+				},
+			},
+		],
+	},
+};
